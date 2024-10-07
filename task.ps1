4a8a1c29f364215be26168581972e424c0fb9602
Yaroslav <ya.yaroslav27@gmail.com>
2024-10-07 11:47:18 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -3,10 +3,13 @@ $resourceGroupName = "mate-azure-task-16"
 
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
+
 $webSubnetName = "webservers"
 $webSubnetIpRange = "10.20.30.0/26"
+
 $dbSubnetName = "database"
 $dbSubnetIpRange = "10.20.30.64/26"
+
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
 
@@ -15,19 +18,8 @@ Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 Write-Host "Creating web network security group..."
-$webNsgRuleHttp = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTP" `
-    -Description "Allow HTTP at port 80" `
-    -Access "Allow" `
-    -Protocol "Tcp" `
-    -Direction "Inbound" `
-    -Priority 110 `
-    -SourceAddressPrefix "Internet" `
-    -SourcePortRange "*" `
-    -DestinationAddressPrefix "*" `
-    -DestinationPortRange 80
-
-$webNsgRuleHttps = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTPS" `
-    -Description "Allow HTTPS at port 443" `
+$webNsgRuleHttp = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTP-HTTPS" `
+    -Description "Allow HTTP at port 80 and 443" `
     -Access "Allow" `
     -Protocol "Tcp" `
     -Direction "Inbound" `
@@ -35,12 +27,12 @@ $webNsgRuleHttps = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTPS" `
     -SourceAddressPrefix "Internet" `
     -SourcePortRange "*" `
     -DestinationAddressPrefix "*" `
-    -DestinationPortRange 443
+    -DestinationPortRange 80, 443
 
 $webNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
     -Location $location `
     -Name $webSubnetName `
-    -SecurityRules $webNsgRuleHttp, $webNsgRuleHttps
+    -SecurityRules $webNsgRuleHttp
 
 Write-Host "Creating mngSubnet network security group..."
 $mngNsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name "Allow-SSH" `
@@ -57,24 +49,12 @@ $mngNsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name "Allow-SSH" `
 $mngNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
     -Location $location `
     -Name $mngSubnetName `
-    -SecurityRules $mngNsgRuleSSH `
+    -SecurityRules $mngNsgRuleSSH
 
 Write-Host "Creating dbSubnet network security group..."
-$dbNsgRuleInternalAccess = New-AzNetworkSecurityRuleConfig -Name "Allow-Internal-Access" `
-    -Description "Allow access from webservers subnet" `
-    -Access "Allow" `
-    -Protocol "Tcp" `
-    -Direction "Inbound" `
-    -Priority 100 `
-    -SourceAddressPrefix $webSubnetIpRange `
-    -SourcePortRange "*" `
-    -DestinationAddressPrefix "*" `
-    -DestinationPortRange 3306
-
 $dbNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
     -Location $location `
-    -Name $dbSubnetName `
-    -SecurityRules $dbNsgRuleInternalAccess
+    -Name $dbSubnetName
 
 Write-Host "Creating a virtual network ..."
 $webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName `
