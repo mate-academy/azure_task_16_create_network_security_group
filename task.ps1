c40b7a89fe43bf2da38734fed257bde82b0166da
Yaroslav <ya.yaroslav27@gmail.com>
2024-10-07 11:32:29 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -15,16 +15,80 @@ Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 Write-Host "Creating web network security group..."
-# Write your code for creation of Web NSG here -> 
+$webNsgRuleHttp = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTP" `
+    -Description "Allow HTTP at port 80" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 110 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 80
+
+$webNsgRuleHttps = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTPS" `
+    -Description "Allow HTTPS at port 443" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 100 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 443
+
+$webNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $webSubnetName `
+    -SecurityRules $webNsgRuleHttp, $webNsgRuleHttps
 
 Write-Host "Creating mngSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$mngNsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name "Allow-SSH" `
+    -Description "Allow SSH at port 22" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 150 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 22
+
+$mngNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $mngSubnetName `
+    -SecurityRules $mngNsgRuleSSH `
 
 Write-Host "Creating dbSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$dbNsgRuleInternalAccess = New-AzNetworkSecurityRuleConfig -Name "Allow-Internal-Access" `
+    -Description "Allow access from webservers subnet" `
+    -Access "Allow" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 100 `
+    -SourceAddressPrefix $webSubnetIpRange `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 3306
+
+$dbNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $dbSubnetName `
+    -SecurityRules $dbNsgRuleInternalAccess
 
 Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$dbSubnet,$mngSubnet
+$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName `
+    -AddressPrefix $webSubnetIpRange `
+    -NetworkSecurityGroup $webNsg
+$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName `
+    -AddressPrefix $dbSubnetIpRange `
+    -NetworkSecurityGroup $dbNsg
+$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName `
+    -AddressPrefix $mngSubnetIpRange `
+    -NetworkSecurityGroup $mngNsg
+
+New-AzVirtualNetwork -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $webSubnet,$dbSubnet,$mngSubnet
