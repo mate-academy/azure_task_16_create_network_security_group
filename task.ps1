0ef11d103f508dfedb2111022c260feb902c7458
KempachiDZ <alexmelnyk48370@gmail.com>
2025-07-08 18:41:06 +0300

    solve task

--- task.ps1
+++ task.ps1
@@ -3,28 +3,85 @@ $resourceGroupName = "mate-azure-task-16"
 
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
+
 $webSubnetName = "webservers"
 $webSubnetIpRange = "10.20.30.0/26"
+
 $dbSubnetName = "database"
 $dbSubnetIpRange = "10.20.30.64/26"
+
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
-
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
+# --- NSG for Webservers ---
 Write-Host "Creating web network security group..."
-# Write your code for creation of Web NSG here -> 
+$webNSGRuleHttpHttps = New-AzNetworkSecurityRuleConfig -Name "Allow-HTTP-HTTPS" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 100 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 80,443 `
+    -Access "Allow"
 
-Write-Host "Creating mngSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$webNSG = New-AzNetworkSecurityGroup `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $webSubnetName `
+    -SecurityRules $webNSGRuleHttpHttps
 
-Write-Host "Creating dbSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+# --- NSG for Management ---
+Write-Host "Creating management network security group..."
+$mngNSGRuleSSH = New-AzNetworkSecurityRuleConfig -Name "Allow-SSH" `
+    -Protocol "Tcp" `
+    -Direction "Inbound" `
+    -Priority 100 `
+    -SourceAddressPrefix "Internet" `
+    -SourcePortRange "*" `
+    -DestinationAddressPrefix "*" `
+    -DestinationPortRange 22 `
+    -Access "Allow"
 
-Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$dbSubnet,$mngSubnet
+$mngNSG = New-AzNetworkSecurityGroup `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $mngSubnetName `
+    -SecurityRules $mngNSGRuleSSH
+
+# --- NSG for Database ---
+Write-Host "Creating database network security group..."
+
+$dbNSG = New-AzNetworkSecurityGroup `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Name $dbSubnetName
+
+# --- Create subnets with attached NSGs ---
+Write-Host "Creating a virtual network with subnets..."
+
+$webSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $webSubnetName `
+    -AddressPrefix $webSubnetIpRange `
+    -NetworkSecurityGroup $webNSG
+
+$dbSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $dbSubnetName `
+    -AddressPrefix $dbSubnetIpRange `
+    -NetworkSecurityGroup $dbNSG
+
+$mngSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $mngSubnetName `
+    -AddressPrefix $mngSubnetIpRange `
+    -NetworkSecurityGroup $mngNSG
+
+New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $webSubnet, $dbSubnet, $mngSubnet
+
