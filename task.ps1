be32999c4d8aa14605736f87b574e44976f5612c
Dima-shima <shifrin1970@gmail.com>
2026-08-21 18:27:05 +0300

    Completed

--- task.ps1
+++ task.ps1
@@ -1,30 +1,95 @@
-$location = "uksouth"
+$location = "denmarkeast"
 $resourceGroupName = "mate-azure-task-16"
 
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
+
 $webSubnetName = "webservers"
 $webSubnetIpRange = "10.20.30.0/26"
+
 $dbSubnetName = "database"
 $dbSubnetIpRange = "10.20.30.64/26"
+
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
 
 
 Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location
+
 
 Write-Host "Creating web network security group..."
-# Write your code for creation of Web NSG here -> 
 
-Write-Host "Creating mngSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$webRule = New-AzNetworkSecurityRuleConfig `
+    -Name "Allow-Web" `
+    -Description "Allow HTTP and HTTPS from Internet" `
+    -Access Allow `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 100 `
+    -SourceAddressPrefix Internet `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 80,443
 
-Write-Host "Creating dbSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$webNsg = New-AzNetworkSecurityGroup `
+    -Name $webSubnetName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -SecurityRules $webRule
+
+
+Write-Host "Creating management network security group..."
+
+$mngSshRule = New-AzNetworkSecurityRuleConfig `
+    -Name "Allow-SSH" `
+    -Description "Allow SSH from Internet" `
+    -Access Allow `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 100 `
+    -SourceAddressPrefix Internet `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 22
+
+$mngNsg = New-AzNetworkSecurityGroup `
+    -Name $mngSubnetName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -SecurityRules $mngSshRule
+
+
+Write-Host "Creating database network security group..."
+
+$dbNsg = New-AzNetworkSecurityGroup `
+    -Name $dbSubnetName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location
+
 
 Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$dbSubnet,$mngSubnet
+
+$webSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $webSubnetName `
+    -AddressPrefix $webSubnetIpRange `
+    -NetworkSecurityGroupId $webNsg.Id
+
+$dbSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $dbSubnetName `
+    -AddressPrefix $dbSubnetIpRange `
+    -NetworkSecurityGroupId $dbNsg.Id
+
+$mngSubnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $mngSubnetName `
+    -AddressPrefix $mngSubnetIpRange `
+    -NetworkSecurityGroupId $mngNsg.Id
+
+New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $webSubnet, $dbSubnet, $mngSubnet
